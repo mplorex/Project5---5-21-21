095df406ce5bbeb278b62c5391be3047d58de9b5
michael posselt <michaelposselt@outlook.com>
2021-06-01 11:25:38 -0500

    website

---
+++ Javascript/styles.js
@@ -0,0 +1,9 @@
+class product {
+    constructor(title, description, price) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    }
+}
+
+let firstProduct = new product ('Wood', 'Fine piece of wood', '$6.99' )
