8e515b07d8f5c57435757f2c0aba4ed21d29b895
michael posselt <michaelposselt@outlook.com>
2021-06-03 12:56:09 -0500

    product card JS creation

--- Javascript/styles.js
+++ Javascript/styles.js
@@ -1,9 +1,33 @@
-class product {
-    constructor(title, description, price) {
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    }
+const bigTeddy = {
+    price: 19.99,
+    name: 'Big teddy'
+}
+const bigTeddy1 = {
+    price: 19.98,
+    name: 'Big Teddy 1'
 }
 
-let firstProduct = new product ('Wood', 'Fine piece of wood', '$6.99' )
+const products = [
+    bigTeddy,
+    bigTeddy2,
+    {
+        name: 'Big Teddy'
+    }
+]
+
+for (let i=0; i < products.length; i++) {
+    const card = document.createElement ('div')
+    const cardBody = document.createElement('div')
+    const cardTitle = document.createElement('h5')
+
+    card.setAttribute('class', 'card')
+    cardBody.setAttribute('class', 'card-body')
+    cardTitle.setAttribute('class', 'card-title')
+
+    const cardTitleContent = document.createTextNode("bigTeddy1")
+    cardTitle.appendChild(cardTitleContent)
+    cardBody.appendChild(cardTitle)
+    card.appendChild(careBody)
+
+    document.body.appendChild(card)
+}
