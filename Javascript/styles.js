a4fde82cf1939dbf3bd0d6d8487f8f7d6fbdc944
michael posselt <michaelposselt@outlook.com>
2021-06-06 11:59:55 -0500

    adding ID

--- Javascript/styles.js
+++ Javascript/styles.js
@@ -1,6 +1,7 @@
 const bigTeddy = {
+    name: 'Big teddy',
+    description: 'Test',
     price: 19.99,
-    name: 'Big teddy'
 }
 const bigTeddy1 = {
     price: 19.98,
@@ -9,25 +10,32 @@ const bigTeddy1 = {
 
 const products = [
     bigTeddy,
-    bigTeddy2,
-    {
-        name: 'Big Teddy'
-    }
+    bigTeddy1,
+    
 ]
 
-for (let i=0; i < products.length; i++) {
-    const card = document.createElement ('div')
-    const cardBody = document.createElement('div')
-    const cardTitle = document.createElement('h5')
+let product = document.querySelector('#bigTeddy');
+paragraph.innerText = 'name, description, price'
 
-    card.setAttribute('class', 'card')
-    cardBody.setAttribute('class', 'card-body')
-    cardTitle.setAttribute('class', 'card-title')
+const api = 'http://localhost:3000/api/teddies';
 
-    const cardTitleContent = document.createTextNode("bigTeddy1")
-    cardTitle.appendChild(cardTitleContent)
-    cardBody.appendChild(cardTitle)
-    card.appendChild(careBody)
+fetch()
+    .then(response => respond.json())
+    .then(data => {
+        for (let i=0; i < products.length; i++) {
+            const card = document.createElement ('div')
+            const cardBody = document.createElement('div')
+            const cardTitle = document.createElement('h5')
 
-    document.body.appendChild(card)
-}
+            card.setAttribute('class', 'card')
+            cardBody.setAttribute('class', 'card-body')
+            cardTitle.setAttribute('class', 'card-title')
+
+            const cardTitleContent = document.createTextNode("bigTeddy1")
+            cardTitle.appendChild(cardTitleContent)
+            cardBody.appendChild(cardTitle)
+            card.appendChild(careBody)
+
+            document.body.appendChild(card)
+        }
+})
